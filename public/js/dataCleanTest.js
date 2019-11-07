c7750d4547a71519bc276cae22a498ebef6f9495
Robin Frugte <robin.frugte@hotmail.com>
2019-11-07 19:48:18 +0100

    Removed d3 from npm and added script

--- public/js/dataCleanTest.js
+++ public/js/dataCleanTest.js
@@ -1,8 +1,2 @@
-import { csv } from 'd3';
-
-let splitData = enqueteData.split("; ")
-
-csv("https://gist.github.com/RobinFrugte97/a5e17cb2420bb1a671ff3912fb1d8cda")
-    .then(function (data) {
-        console.log(data);
-    });
+d3.csv("https://gist.github.com/RobinFrugte97/a5e17cb2420bb1a671ff3912fb1d8cda")
+    .then((data) => console.log(data))
